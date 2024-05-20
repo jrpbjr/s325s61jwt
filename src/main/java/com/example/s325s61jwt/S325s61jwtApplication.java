e043718dd7db79f72fc1a022f0f49636ee8784a6
jrpbjr <jrpbjr@gmail.com>
2024-05-20 16:07:06 -0300

    first commit

---
+++ src/main/java/com/example/s325s61jwt/S325s61jwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.s325s61jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S325s61jwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S325s61jwtApplication.class, args);
+    }
+
+}
