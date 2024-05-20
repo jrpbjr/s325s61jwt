e1877cf3ebdb69beecbfc237bf16adeee7720e73
jrpbjr <jrpbjr@gmail.com>
2024-05-20 16:26:04 -0300

    adicionando repositorio para buscar username.

---
+++ src/main/java/com/example/s325s61jwt/repositories/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.example.s325s61jwt.repositories;
+
+import com.example.s325s61jwt.entities.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+}
