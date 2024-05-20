cbfe50147926121b3528adb0e90a15425907c227
jrpbjr <jrpbjr@gmail.com>
2024-05-20 16:14:35 -0300

    adicionando entitidades de autenticacao e login.

---
+++ src/main/java/com/example/s325s61jwt/entities/AuthRequest.java
@@ -0,0 +1,12 @@
+package com.example.s325s61jwt.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String username;
+    private String password;
+}
