6662a31ff1d9554f2015c89eae1511f6f88a077a
jrpbjr <jrpbjr@gmail.com>
2024-05-20 16:57:07 -0300

    adicionando controller para providenciar o registro do usuario e autenticacao.

---
+++ src/main/java/com/example/s325s61jwt/Controllers/UserController.java
@@ -0,0 +1,48 @@
+package com.example.s325s61jwt.Controllers;
+
+import com.example.s325s61jwt.Services.JwtService;
+import com.example.s325s61jwt.Services.UserInfoService;
+import com.example.s325s61jwt.entities.AuthRequest;
+import com.example.s325s61jwt.entities.UserInfo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    private final UserInfoService service;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    UserController(UserInfoService service, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.service = service;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> addNewUser(@RequestBody UserInfo userInfo) {
+        String response = service.addUser(userInfo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/generateToken")
+    public ResponseEntity<String> authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            String token = jwtService.generateToken(authRequest.getUsername());
+            return ResponseEntity.ok(token);
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
